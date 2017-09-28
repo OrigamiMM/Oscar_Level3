7d2a20de7d0099a2ad2e26dd376289a318ebf4ad
OrigamiMM <league@Mac-mini-3.local>
2017-09-28 16:54:50 -0700

    9-28-17
    Working on the HashMap Stuff, on part 2 of it and made some progress on
    it.

--- src/IntroToHashMaps/LogSearch.java
+++ src/IntroToHashMaps/LogSearch.java
@@ -6,20 +6,31 @@ import java.util.HashMap;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class LogSearch implements ActionListener {
-	HashMap <Integer, String> Logs = new HashMap <Integer, String>();
+	HashMap<Integer, String> Logs = new HashMap<Integer, String>();
 	JFrame frame = new JFrame();
 	JPanel pan = new JPanel();
 	JButton entry = new JButton("Enter a name");
 	JButton search = new JButton("Search the log");
 	JButton list = new JButton("List Name");
-	
+	String name = "";
+	int idd = 0;
+	String n = "";
+	int ii = 0;
+
 	public static void main(String[] args) {
 		LogSearch logz = new LogSearch();
 		logz.GUI();
+		logz.addlog();
 	}
+
+	void addlog() {
+		Logs.put(idd, name);
+	}
+
 	void GUI() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(pan);
@@ -36,33 +47,46 @@ public class LogSearch implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		
+		if (arg0.getSource() == entry) {
+			name = JOptionPane.showInputDialog("Please Enter a name");
+			String idz = JOptionPane.showInputDialog("Link with an id");
+			idd = Integer.parseInt(idz);
+			addlog();
+		}
+		if (arg0.getSource() == search) {
+
+		}
+		if (arg0.getSource() == list) {
+			for (int i = 0; i < Logs.size()/2; i++) {
+				for (String s : Logs.values()) {
+					n = s;
+				}
+				for (Integer j : Logs.keySet()) {
+					ii = j;
+				}
+				System.out.println(ii + " " + n);
+			}
+		}
 	}
 }
-  /* 
-	 * Crate a HashMap of Integers for the keys and Strings for the values.
-	 * Create a GUI with three buttons. 
-	 * Button 1: Add Entry
-	 * 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
-	 * 				After an ID is entered, use another input dialog to ask the user to enter a name.
-	 * 				Add this information as a new entry to your HashMap.
-	 * 
-	 * Button 2: Search by ID
-	 * 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
-	 * 				If that ID exists, display that name to the user.
-	 * 				Otherwise, tell the user that that entry does not exist.
-	 * 
-	 * Button 3: View List
-	 * 				When this button is clicked, display the entire list in a message dialog in the following format:
-	 * 				ID: 123  Name: Harry Howard
-	 * 				ID: 245  Name: Polly Powers
-	 * 				ID: 433  Name: Oliver Ortega
-	 * 				etc...
-	 * 
-	 * When this is complete, add a fourth button to your window.
-	 * Button 4: Remove Entry
-	 * 				When this button is clicked, prompt the user to enter an ID using an input dialog.
-	 * 				If this ID exists in the HashMap, remove it. Otherwise, notify the user that the ID
-	 * 				is not in the list. 
-	 *
-	 * */
+/*
+ * Crate a HashMap of Integers for the keys and Strings for the values. Create a
+ * GUI with three buttons. Button 1: Add Entry When this button is clicked, use
+ * an input dialog to ask the user to enter an ID number. After an ID is
+ * entered, use another input dialog to ask the user to enter a name. Add this
+ * information as a new entry to your HashMap.
+ * 
+ * Button 2: Search by ID When this button is clicked, use an input dialog to
+ * ask the user to enter an ID number. If that ID exists, display that name to
+ * the user. Otherwise, tell the user that that entry does not exist.
+ * 
+ * Button 3: View List When this button is clicked, display the entire list in a
+ * message dialog in the following format: ID: 123 Name: Harry Howard ID: 245
+ * Name: Polly Powers ID: 433 Name: Oliver Ortega etc...
+ * 
+ * When this is complete, add a fourth button to your window. Button 4: Remove
+ * Entry When this button is clicked, prompt the user to enter an ID using an
+ * input dialog. If this ID exists in the HashMap, remove it. Otherwise, notify
+ * the user that the ID is not in the list.
+ *
+ */
